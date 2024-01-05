a985cfbe8422f056b0643c413b301196f62f0057
Wendell Matheus <125501486+WendellMat@users.noreply.github.com>
2024-01-04 23:58:51 -0300

    Add files via upload

---
+++ dio/arrays_e_estruturas_de_repeticao_em_javascript/aula03/numerosPares.js
@@ -0,0 +1,7 @@
+const num = [5, 54, 1, 9, 6, 78, 33, 6, 0, 784, 331, 89, 22];
+
+for (let k=0; k<num.length; k++) {
+    if (num[k] % 2 === 0) {
+        console.log(num[k] + ' é par');
+    }
+}
---
+++ dio/arrays_e_estruturas_de_repeticao_em_javascript/aula03/tabuada.js
@@ -0,0 +1,5 @@
+const num = 36;
+
+for (let k=1; k<=10; k++) {
+    console.log(num + ' x ' + k + ' = ' + (num * k));
+}
